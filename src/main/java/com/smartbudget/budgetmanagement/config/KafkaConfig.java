e95d2fd07cd70e0ba23cda7a710812098a9f956a
georgiken <georgiy.voronin.05@mail.ru>
2024-05-23 01:59:28 +0300

    kafka group name changed

--- src/main/java/com/smartbudget/budgetmanagement/config/KafkaConfig.java
+++ src/main/java/com/smartbudget/budgetmanagement/config/KafkaConfig.java
@@ -21,7 +21,7 @@ public class KafkaConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "budget-service-group");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "budget-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
